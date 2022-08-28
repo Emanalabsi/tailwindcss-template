7cb4b2b5d25576e9ab6ecd2228b2502814fddd79
EmanAlabsi <emanabsi30@gmail.com>
2022-08-28 10:56:43 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        blue: "#26335D",
+        orange: "#E77C40",
+        sky: "#8EC2F2",
+        danger: "#E74040",
+        secondaryblue: "#0061FF",
+      },
+    },
+  },
+  plugins: [],
+};
